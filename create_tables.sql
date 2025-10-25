ef1915936ff4b3f642dbd0b79da014841873670d
Aman Sunesh <as18181@nyu.edu>
2025-10-25 13:22:28 -0400

    Create database tables for airline system
    Added tables for Airline, Airplane, AirlineStaff, StaffPhoneNo, Airport, Flight, and Customer with appropriate fields and constraints.

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,67 @@
+CREATE TABLE Airline(
+  airline_name VARCHAR (50) PRIMARY KEY
+  );
 
+CREATE TABLE Airplane(
+  airplane_id VARCHAR(20),
+  airline_name VARCHAR(50),
+  seat_capacity INT,
+  manufacturer VARCHAR(100),
+  age INT,
+  PRIMARY KEY (airplane_id, airline_name),
+  FOREIGN KEY (airline_name) REFERENCES Airline(airline_name)
+  );
+
+CREATE TABLE AirlineStaff(
+  username VARCHAR(50);
+  password VARCHAR(255),
+  airline_name VARCHAR(50),
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  date_of_birth DATE,
+  email VARCHAR(255),
+  PRIMARY KEY (username),
+  FOREIGN KEY airline_name REFERENCES Airline(airline_name)
+);
+
+CREATE TABLE StaffPhoneNo(
+  username VARCHAR(50),
+  phone_number VARCHAR(25),
+  PRIMARY KEY (username, phone_number),
+  FOREIGN KEY username REFERENCES AirlineStaff(username)
+);
+
+CREATE TABLE Airport(
+  code CHAR(3),
+  city VARCHAR(200),
+  country VARCHAR(60),
+  airport_type ENUM('domestic','international','both'),
+  PRIMARY KEY (code)
+);
+
+CREATE TABLE Flight(
+  flight_no VARCHAR(10),
+  dep_datetime DATETIME,
+  airplane_id VARCHAR(20)
+  airline_name VARCHAR (50)
+  dep_airport_code CHAR(3)
+  arr_airport_code CHAR(3)
+  arr_datetime DATETIME,
+  status ENUM('on-time','delayed') NOT NULL DEFAULT 'on-time'
+  base_price DECIMAL(10,2) CHECK (base_price >= 0)
+  PRIMARY KEY (flight_no, dep_datetime, airplane_id, airline_name),
+  FOREIGN KEY airline_name REFERENCES Airline(airline_name),
+  FOREIGN KEY dep_airport_code REFERENCES Airport(code),
+  FOREIGN KEY arr_airport_code REFERENCES Airport(code),
+);
+
+
+CREATE TABLE Customer(
+  email VARCHAR(255),
+  name VARCHAR(100),
+  
+
+
+
+  
+  
