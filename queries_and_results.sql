f72a3e435978a148de62186793a19b8ad24daa2c
Aman Sunesh <as18181@nyu.edu>
2025-10-25 16:29:15 -0400

    Add SQL queries for future and delayed flights
    Add final version of SQL queries to retrieve future flights, delayed flights, ticket buyers, and Jet Blue airplanes

--- queries_and_results.sql
+++ queries_and_results.sql
@@ -1,1 +1,21 @@
+-- Show all the future flights in the system.
+SELECT * FROM Flight 
+WHERE dep_datetime > NOW()
+ORDER BY dep_datetime;
 
+-- Show all of the delayed flights in the system.
+SELECT * FROM Flight 
+WHERE status = 'delayed'
+ORDER BY dep_datetime;
+
+--  Show the customer names who bought the tickets.
+SELECT DISTINCT c.name 
+FROM Customer AS c 
+JOIN Ticket AS t 
+ON c.email = t.customer_email
+ORDER BY c.name;
+
+-- Show all the airplanes owned by the airline Jet Blue.
+SELECT * FROM Airplane 
+WHERE airline_name = 'Jet Blue'
+ORDER BY airplane_id;
